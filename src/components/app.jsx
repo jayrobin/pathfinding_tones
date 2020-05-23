19151c9336c07307d1bc527e20446642ec7a6cb5
james_robinson <james.robinson@airbnb.com>
2020-05-23 11:11:51 -0700

    Initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div style={styles.container}>
+        <h1>Hello, world</h1>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+};
